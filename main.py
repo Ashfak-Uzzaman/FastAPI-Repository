c4a0ee31eb27a76201bedfd4e2310d37bb6d706c
Ashfak Uzzaman <mdashfak0508@gmail.com>
2026-05-19 07:00:00 +0600

    StaticFiles

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # uv run fastapi dev main.py
 
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 
 app=FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+# The line bellow mounts a separate ASGI application (StaticFiles) inside the main FastAPI app.
+app.mount("/static", StaticFiles(directory="static"), name="assets") 
+# "/static" is the URL path
+# Here StaticFiles(directory="static") is a separate ASGI application.
+# name="static" gives the mounted app a route name.
 
 
 posts: list[dict] = [
@@ -25,7 +32,7 @@ posts: list[dict] = [
     },
     {
         "id": 3,
-        "author": "Jane Doe",
+        "author": "Subin",
         "title": "Python is Great for Web Development",
         "content": "Python is a great language for web development, and FastAPI makes it even better. Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nulla similique animi quo quidem quia excepturi, sunt magni aut voluptatum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis nulla similique animi quo quidem quia excepturi, sunt magni aut voluptatum!",
         "date_posted": "April 21, 2025",
@@ -34,14 +41,13 @@ posts: list[dict] = [
 
 
 
-@app.get("/", include_in_schema=False, name="home")
-@app.get("/home", include_in_schema=False, name="home")
-def home(request:Request):
+@app.get("/")
+async def home(request: Request):
     return templates.TemplateResponse(
-        request,
-        "home.html",
-        {"posts":posts, "title": "Home"}
-    )
+    request,
+    "home.html",
+    {"request": request}
+)
     
     
     
@@ -55,26 +61,3 @@ def show_posts(request: Request):
         {"posts": posts, "title": "Posts"}
     )
 
-'''
-Why use `url_for()` and name="somthing" instead of hardcoding URLs?
-
-Instead of writing:
-
-<a href="/posts">
-
-you write:
-
-<a href="{{ url_for('posts') }}">
-
-because if you later change the route:
-
-@app.get("/all-posts", name="posts")
-
-your template still works automatically.
-
-url_for() will now generate:
-
-<a href="/all-posts">
-
-without changing the HTML template.
-'''
